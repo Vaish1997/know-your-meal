54ca488276c18b7c225e31e1120a82535fca281b
vaishali Sridhar <sridharjayashree8497@gmail.com>
2020-08-01 18:45:41 -0400

    Updates

---
+++ precache-manifest.cd5fe916dd68e6103de27860be31cb2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2bd79bc1882604976e5749433ad4333",
+    "url": "/know-your-meal/index.html"
+  },
+  {
+    "revision": "395f343acc1095b4f159",
+    "url": "/know-your-meal/static/css/main.6f02e31d.chunk.css"
+  },
+  {
+    "revision": "57c2b76a8bb2e9fa250a",
+    "url": "/know-your-meal/static/js/2.df755e86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/know-your-meal/static/js/2.df755e86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "395f343acc1095b4f159",
+    "url": "/know-your-meal/static/js/main.428e5e2a.chunk.js"
+  },
+  {
+    "revision": "c0c9e2bd17e92fb9e4c1",
+    "url": "/know-your-meal/static/js/runtime-main.ff333199.js"
+  }
+]);
